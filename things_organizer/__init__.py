da0764c82092862e178df80a0f0639c92cff0e66
yeyeto2788 <juanernestobiondi@gmail.com>
2019-03-25 16:20:34 +0100

    Add fix for platform base url for database

--- things_organizer/__init__.py
+++ things_organizer/__init__.py
@@ -21,14 +21,13 @@ from things_organizer.api import categories, storages, tags, things
 
 
 app = flask.Flask(__name__, static_url_path="/static")
+database_path = os.path.abspath(os.path.join(utils.DB_PATH, "things_organizer.db"))
 
 if sys.platform == 'linux':
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////{}".format(os.path.join(
-        utils.DB_PATH, "things_organizer.db"))
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////{}".format(database_path)
     print(os.path.join(utils.DB_PATH, "things_organizer.db"))
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(os.path.join(
-        utils.DB_PATH, "things_organizer.db"))
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(database_path)
     print(os.path.join(utils.DB_PATH, "things_organizer.db"))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
