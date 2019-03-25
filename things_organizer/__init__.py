9c853f6980369135f78de107d99ff8e7c2d43a75
yeyeto2788 <juanernestobiondi@gmail.com>
2019-03-25 15:30:43 +0100

    Fix error for database

--- things_organizer/__init__.py
+++ things_organizer/__init__.py
@@ -20,8 +20,8 @@ from things_organizer.api import categories, storages, tags, things
 
 
 app = flask.Flask(__name__, static_url_path="/static")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(os.path.join(utils.DB_PATH,
-                                                                           "things_organizer.db"))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////{}".format(os.path.join(utils.DB_PATH,
+                                                                            "things_organizer.db"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.urandom(16)
 
