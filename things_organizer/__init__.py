1ac3adf8ae47f17f47cd828a7efc71e59d5ce0de
yeyeto2788 <juanernestobiondi@gmail.com>
2019-04-15 13:41:59 +0200

    Fix some linter warnings.

--- things_organizer/__init__.py
+++ things_organizer/__init__.py
@@ -21,14 +21,14 @@ from things_organizer.api import categories, storages, tags, things
 
 
 app = flask.Flask(__name__, static_url_path="/static")
-database_path = os.path.abspath(os.path.join(utils.DB_PATH, "things_organizer.db"))
+DATABASE_PATH = os.path.abspath(os.path.join(utils.DB_PATH, "things_organizer.db"))
 
 if sys.platform == 'linux':
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////{}".format(database_path)
-    print(database_path)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////{}".format(DATABASE_PATH)
+    print(DATABASE_PATH)
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(database_path)
-    print(database_path)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(DATABASE_PATH)
+    print(DATABASE_PATH)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.urandom(16)
